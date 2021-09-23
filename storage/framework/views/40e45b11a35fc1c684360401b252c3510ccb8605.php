cc528041e96ba898533896d6bbd749cee0914a2b
saransamy85 <85836613+saransamy85@users.noreply.github.com>
2021-09-24 05:17:43 +0530

    Add files via upload

---
+++ storage/framework/views/40e45b11a35fc1c684360401b252c3510ccb8605.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="<?php echo e(asset('css/style.css')); ?>">
+    <title>Login</title>
+</head>
+
+<body>
+    <section id="cont">
+        <div class="container">
+            <div class="row">
+                <div class="col" id="logo">
+                    <img src="<?php echo e(asset('images/1.png')); ?>" height="100%" width="100%">
+                </div>
+                <div class="col" id="login-form">
+                    <form action="<?php echo e(route('check')); ?>" method="POST">
+                        <?php if(Session::get('fail')): ?>
+                        <div class="alert alert-danger">
+                            <?php echo e(Session::get('fail')); ?>
+
+                        </div>
+                        <?php endif; ?>
+                        <?php echo csrf_field(); ?>
+                        <div id="inner-form" class="w-100">
+                            <h4 class="text-center">LOGIN</h4>
+                            <div class="form-group">
+                                <label for="username">username</label>
+                                <input type="text" class="form-control" name="uname">
+                                <span class="text-danger"><?php $__errorArgs = ['uname'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?><?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+                            </div>
+                            <br>
+                            <div class="form-group">
+                                <label for="password">password</label>
+                                <input type="password" class="form-control" name="pwd">
+                                <span class="text-danger"><?php $__errorArgs = ['pwd'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?><?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+                            </div>
+                            <br>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-success w-100" value="login">
+                            </div>
+                            <br>
+                            <a href="<?php echo e(route('register')); ?>">I don't have an account. Create new</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html><?php /**PATH E:\Laravel_Tutorials\loginex\resources\views/login.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/952858382bd6e0e1f1c9942e7903300b073f279b.php
@@ -0,0 +1,13 @@
+<h1>Welcome mr admin</h1>
+
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Email</th>
+    </tr>
+    <tr>
+        <td><?php echo e($Lui['name']); ?></td>
+        <td><?php echo e($Lui['email']); ?></td>
+        <td><a href="">Log Out</a></td>
+    </tr>
+</table><?php /**PATH E:\Laravel_Tutorials\loginex\resources\views/admin/dashboard.blade.php ENDPATH**/ ?>
